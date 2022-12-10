bd9f991af0d1d43ab19165a8b829647d5c534143
ashok159 <ashoksurujdeo@gmail.com>
2022-12-09 21:25:18 -0500

    Product Page Render

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -5,27 +5,33 @@ import Cart from "../components/Cart";
 import Sidebar from "../components/Sidebar";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function ProductPage() {
   const { id } = useParams();
   const [itemData, setItemdata] = useState([]);
+  const [image, setImage] = useState([]);
 
-  useEffect(()=>{
-        axios.get(`/item/${id}`)
-          .then(response => {
-            console.log(response.data)
-        setItemdata(response.data);
-      })
-
-  }, [])
+  useEffect(() => {
+    axios.get(`/item/${id}`).then((response) => {
+      console.log(response.data);
+      setItemdata(response.data);
+      setImage(response.data.images);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <main>
+      <div className="product_page">
+        <h3>Brand: {itemData.brand}</h3>
+        <img src={image[0]} alt="product" />
         <h1>{itemData.title}</h1>
-        <p>{itemData.description}</p>
-      </main>
+        <h2>{itemData.description}</h2>
+        <h3>Rating: {itemData.rating}/5</h3>
+        <h3>${itemData.price}</h3>
+        <h3>Items Remaining: {itemData.stock}</h3>
+        <button>Add to Cart</button>
+      </div>
       <Footer />
     </div>
   );
